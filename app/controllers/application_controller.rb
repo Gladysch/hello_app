19e5f893985b6579df0f6e75e4c968e82fcee8cb
GladysCh <gchecasaca@lexuseditores.com>
2017-06-02 10:54:58 -0500

    La pagina principal es Hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+  	render html: "Hello World, I'm Gladys, is beatifull begin with rails"
+  end
 end
